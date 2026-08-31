fc4aa37e1bb46345d1b6aff6374d2042df8d83af
lomedx <aj4555822@gmail.com>
2026-08-31 03:34:50 +0300

    Increment CACHE_NAME for version update
    Updated CACHE_NAME to force cache refresh.

--- OneSignalSDKWorker.js
+++ OneSignalSDKWorker.js
@@ -2,7 +2,7 @@
 importScripts('https://cdn.onesignal.com/sdks/web/v16/OneSignalSDK.sw.js');
 
 // 2. إعدادات الكاش والتخزين الخاص بموقعك (PWA)
-const CACHE_NAME = 'raheba-med-v58'; // تم رفع الرقم لإجبار التحديث
+const CACHE_NAME = 'raheba-med-v59'; // تم رفع الرقم لإجبار التحديث
 const CORE_ASSETS = [
   './',
   './index.html',
